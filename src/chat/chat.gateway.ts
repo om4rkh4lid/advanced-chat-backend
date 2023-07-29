dfbd227b5a41a5c2c25dc4682a59f4dc52555324
Omar Khalid <xom4rxkh4lidx@gmail.com>
2023-07-29 18:05:47 +0300

    Private messaging

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -14,24 +14,35 @@ import sessionMiddleware, { removeSession, sessions } from './middleware/session
 import { ChatMessage } from './entities/ChatMessage';
 
 @WebSocketGateway({ cors: true })
-export class ChatGateway implements OnGatewayInit, OnGatewayConnection {
+export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() server: Server;
 
   constructor(private chatService: ChatService) { }
-
+  
   afterInit(server: Server) {
     server.use(sessionMiddleware);
   }
-
-  handleConnection(client: Socket, ...args: any[]) {
+  
+  async handleConnection(client: Socket, ...args: any[]) {
     client.emit('sessionCreated', client.session);
     client.join(`user#${client.session.userId}`);
-    client.emit('activeUsers', sessions.map(session => session.userId));
+    const socketsInTheRoom = (await this.server.in(`user#${client.session.userId}`).fetchSockets()).length;
+    console.log(`Sockets in room #${client.session.userId}:`, socketsInTheRoom);
+    this.server.emit('activeUsers', sessions.map(session => session.userId));
   }
-
+  
   @SubscribeMessage('chatMessage')
   handleNewChatMessage(@MessageBody() message: ChatMessage, @ConnectedSocket() client: Socket): boolean {
     this.server.to(`user#${message.from}`).to(`user#${message.to}`).emit('chatMessage', message);
     return true;
   }
+
+  async handleDisconnect(client: Socket) {
+    const socketsInTheRoom = (await this.server.in(`user#${client.session.userId}`).fetchSockets()).length;
+    if (socketsInTheRoom === 0) {
+      removeSession(client.session.userId);
+      this.server.emit('activeUsers', sessions.map(session => session.userId));
+    }
+  }
+
 }
